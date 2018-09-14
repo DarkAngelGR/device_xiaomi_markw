e962456f09f61ba01c0b14f73114b72184e89ac1
Yash-Garg <ben10.yashgarg@gmail.com>
2018-09-14 04:48:59 +0000

    markw: Switch to common Power HAL instead of lineage
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- device.mk
+++ device.mk
@@ -426,8 +426,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
-    
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml \
 
